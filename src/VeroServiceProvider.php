b3d90fe0308ecdb27a01af17c0ecfb095852011a
Sam Littler <sam@tickr.co.uk>
2019-11-06 17:24:07 +0000

    Use different unlying dependency

--- src/VeroServiceProvider.php
+++ src/VeroServiceProvider.php
@@ -4,17 +4,18 @@ namespace SamLittler\LaravelVero;
 
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use SamLittler\Vero\Vero;
 
 class VeroServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(\Vero::class, function ($app) {
-            return new \Vero(self::config('auth_token'));
+        $this->app->singleton(Vero::class, function ($app) {
+            return new Vero(self::config('auth_token'));
         });
 
         $this->app->bind('vero', function () {
-            return $this->app->make(\Vero::class);
+            return $this->app->make(Vero::class);
         });
     }
 
